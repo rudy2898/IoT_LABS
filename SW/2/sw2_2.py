fa28fbba0057399dc79283e7e8b2c48a2b656235
pasquale <pasquale_rinaldi@hotmail.it>
2020-07-06 18:31:42 +0200

    .

--- SW/2/sw2_2.py
+++ SW/2/sw2_2.py
@@ -1,7 +1,15 @@
 import requests
 
 if __name__ == "__main__":
-    code = input("Inserire il codice corrispondente all'operazione desiderata:\n1->Stampa il message broker\n2->Lista utenti\n3->Lista dispostivi\n4->Cerca utente\n5->Cerca dispositivo\n")
+    code = input(
+        """Inserire il codice corrispondente all'operazione desiderata:\n
+        1->Stampa il message broker\n
+        2->Lista utenti\n
+        3->Lista dispostivi\n
+        4->Lista servizi\n
+        5->Cerca utente\n
+        6->Cerca dispositivo\n
+        7->Cerca servizio\n""")
     if code=="1":
         r1 = requests.get("http://localhost:8080/ip")
         print(r1)
@@ -12,8 +20,14 @@ if __name__ == "__main__":
         r3 = requests.get("http://localhost:8080/devices/list")
         print(r3)
     elif code=="4":
-        r4 = requests.get("http://localhost:8080/users/search/User1")
+        r4 = requests.get("http://localhost:8080/services/list/")
         print(r4)
     elif code=="5":
-        r5 = requests.get("http://localhost:8080/devices/search/Device1")
+        r5 = requests.get("http://localhost:8080/users/search/User1")
         print(r5)
+    elif code=="6":
+        r6 = requests.get("http://localhost:8080/devices/search/Device1")
+        print(r6)
+    elif code=="7":
+        r7 = requests.get("http://localhost:8080/services/search/Service1")
+        print(r7)
